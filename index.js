6bde5a823d4c2ae30162b247818c249362a9eeef
nagar2817 <nagar.2@iitj.ac.in>
2023-08-21 16:52:09 +0530

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ app.use(express.json());
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
+.then((data) =>
+console.log(`MongoDB connected with server ${data.connection.host}`))
+.catch((err) => console.log(err));
 
 app.use(express.static('static'));
 
