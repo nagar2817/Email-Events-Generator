9c6785ca2a604d2335b972c706ced92b1977a8d4
nagar2817 <nagar.2@iitj.ac.in>
2023-08-21 16:47:18 +0530

    3 files modified, 1 added

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3010;
 const path = require('path');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// Load environment variables from a .env file
+dotenv.config();
+
+// Set up middleware to parse JSON requests
+app.use(express.json());
+
+// Connect to the MongoDB database using Mongoose
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(express.static('static'));
 
